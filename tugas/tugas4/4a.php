834d1429cca170cc8e12e2b19e9decceae250efe
MuzanFera <fauzan.223040027@mail.unpas.ac.id>
2023-03-08 13:30:20 +0700

    mengumpulkan tugas 4

---
+++ tugas/tugas4/4a.php
@@ -0,0 +1,12 @@
+<?php 
+
+// array
+// 6 buah elemen
+
+$i = ['epan', 'cipta', 'akan', 'u', 'asa', 'esok'];
+
+// mencetak array
+
+print "M$i[4] d$i[0]mu di$i[1]kan oleh apa yang kau kerj$i[2] hari ini, b$i[3]kan b$i[5]";
+
+?>
---
+++ tugas/tugas4/4b.php
@@ -0,0 +1,51 @@
+<?php 
+
+// array
+// 6 buah elemen
+
+$hardwares = ['Motherboard', 'Processor', 'Hard Disk', 'PC Coller', 'VGA Card', 'SSD'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perangkat Keras Komputer</title>
+    
+    <style>
+        html {
+            font-family: sans-serif;
+        }
+    </style>
+
+</head>
+<body>
+    
+    <h2>Macam-macam perangkat keras Komputer</h2>
+        <ol>
+            <?php foreach($hardwares as $hardware) { ?>
+                <li>
+                    <?= $hardware ?>
+                </li>
+            <?php } ?>
+        </ol>
+
+    <?php 
+        array_push($hardwares, "Card Reader", "Modem");
+        sort($hardwares);
+    ?>
+
+    <h2>Macam-macam perangkat keras Komputer Baru</h2>
+        <ol>
+            <?php foreach($hardwares as $hardware) { ?>
+                <li>
+                    <?= $hardware ?>
+                </li>
+            <?php } ?>
+        </ol>
+    
+</body>
+</html>
