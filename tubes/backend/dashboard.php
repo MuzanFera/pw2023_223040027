e6ac7619ad39292466733c66dffc3b1a8ed16ca9
MuzanFera <fauzan.223040027@mail.unpas.ac.id>
2023-05-08 22:02:10 +0700

    tubes UI

---
+++ tubes/backend/dashboard.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard | Pemerintah Bandung</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="nav">
+        <div class="logo">
+            <p>
+                <a href="../index.php">
+                    <img class="img-logo" src="../img/logo_bandung.png" alt="logo_bandung">
+                </a>
+            </p>
+        </div>
+
+        <div class="right-links">
+            <a href="../pages/edit.php">Change Profile</a>
+            <a href="../pages/logout.php"> <button class="btn">Log Out</button> </a>
+        </div>
+
+    </div>
+
+    
+    <main>
+
+        <div class="main-box top">
+            <div class="top">
+                <div class="box">
+                    <p>
+                        Hallo <b>....</b>, Selamat Datang
+                    </p>
+                </div>
+                <div class="box">
+                    <p>
+                        Email kamu <b>....@gmail.com</b>.
+                    </p>
+                </div>
+                <div class="bottom">
+                    <div class="box">
+                        <p>
+                            Dan kamu masuk sebagai <b>Admin/User</b>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+    
+</body>
+</html>
---
+++ tubes/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Pemerintah Bandung</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <div class="nav">
+      <div class="logo">
+        <p>
+          <a href="index.php">
+            <img class="img-logo" src="img/logo_bandung.png" alt="logo_bandung"/>
+          </a>
+        </p>
+      </div>
+
+      <div class="right-links">
+        <a href="pages/edit.php">Change Profile</a>
+        <a href="pages/logout.php"> <button class="btn">Log Out</button> </a>
+      </div>
+    </div>
+
+    <!-- jumbotron -->
+    <div class="jumbotron">
+      <div class="container">
+        <h2 class="title-jumb">BANDUNG</h2>
+      </div>
+    </div>
+
+    <!-- etalase bandung -->
+    <div class="etalase">
+      <div class="title-etalase">Etalase Bandung</div>
+      <div class="container">
+        <div class="pic-etalase">
+          <img src="img/dummy-image.png" alt="" />
+          <img src="img/dummy-image.png" alt="" />
+          <img src="img/dummy-image.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <!-- footer -->
+    <footer class="footer">
+      <p>2023. Pemerintah Kota Bandung</p>
+    </footer>
+  </body>
+</html>
