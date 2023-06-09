bc04af3fe81098ca573de98b91954df0cb82ad05
Muhammad Fauzan Febrian Nugraha <121429435+MuzanFera@users.noreply.github.com>
2023-06-09 19:07:55 +0700

    Delete tubes2 directory

--- tubes2/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'function.php';
-$id= $_GET['id'];
-if(hapus($id) > 0) {
-    echo "<script>
-            alert('Akun Berhasil Dihapus!');
-            document.location.href = 'admin/dashboard.admin.php';
-        </script>";
-}
